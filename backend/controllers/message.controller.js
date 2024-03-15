939fb78287833c7595b33f49a852f91c068c1888
Maciej Krasowski <113846964+makranan@users.noreply.github.com>
2024-03-15 10:57:59 +0100

    backend done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -7,31 +7,56 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    // Check if the conversation already exists
     let conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     });
 
+    // If the conversation doesn't exist, create a new one
     if (!conversation) {
-      conversation = new Conversation({
+      conversation = await Conversation.create({
         participants: [senderId, receiverId],
       });
     }
 
+    // Create a new message
     const newMessage = new Message({
       senderId,
       receiverId,
       message,
     });
 
+    // Add the message to the conversation
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
 
-    await newMessage.save();
+    // Save the new message and update conversation
+    await Promise.all([newMessage.save(), conversation.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
     console.log(`Error in sendMessage controller: ${error.message}`);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate('messages');
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log(`Error in getMessage controller: ${error.message}`);
     res.status(500).json({ error: 'Something went wrong' });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectDB from './db/connect.db.js';
 dotenv.config();
 
@@ -15,6 +17,7 @@ app.use(cookieParser()); // to parse incoming requests with cookies
 
 app.use('/api/auth', authRoutes); // use authRoutes for /api/auth
 app.use('/api/messages', messageRoutes); // use messageRoutes for /api/messages
+app.use('/api/users', userRoutes); // use userRoutes for /api/users
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
