7fb5eb584a7a1e95ee2a24f8c037756a7cc403bc
Maciej Krasowski <krasowskinan@gmail.com>
2024-08-04 15:35:52 +0200

    add: search functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,42 @@
+import { useState } from 'react';
 import searchIcon from '../../assets/searchIcon.svg';
+import useConversation from '../../zustand/useConversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 
 const SearchInput = () => {
+  const [input, setInput] = useState('');
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!input) return;
+    if (conversations.length === 0) return;
+    if (input.length < 3) {
+      return toast.error('Search query must be at least 3 characters long');
+    }
+
+    const searchResult = conversations.find((conversation) =>
+      conversation.fullName.toLowerCase().includes(input.toLowerCase())
+    );
+
+    if (searchResult) {
+      setSelectedConversation(searchResult);
+      setInput('');
+    } else {
+      toast.error('No conversation found');
+    }
+  };
+
   return (
-    <form className='flex items-center gap-2'>
+    <form className='flex items-center gap-2' onSubmit={handleSearch}>
       <input
         type='text'
         className='input input-bordered rounded-xxl'
         placeholder='Search...'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button className='btn btn-square'>
         <img src={searchIcon} className='h-auto w-6 filter invert' />
